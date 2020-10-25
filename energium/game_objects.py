93ed877fb230e3e24d14a60334ca1c3a483e5157
James Camacho <camacho.james@gmail.com>
2020-10-24 21:40:47 -0600

    starterbot

---
+++ energium/game_objects.py
@@ -0,0 +1,40 @@
+from typing import List
+from .position import Position
+from .game_constants import GAME_CONSTANTS
+import math
+import sys
+class Base:
+    def __init__(self, team: int, x, y):
+        self.team = team
+        self.pos = Position(x, y)
+
+    def spawn_unit(self):
+        return 'c {} {}'.format(self.pos.x, self.pos.y)
+
+
+class Unit:
+    def __init__(self, team: int, unitid: int, x, y, last_repair_turn, turn):
+        self.team = team
+        self.id = unitid
+        self.pos = Position(x, y)
+        self.last_repair_turn = last_repair_turn
+        self.match_turn = turn
+    
+    def get_breakdown_level(self):
+        """
+        returns the breakdown level of this unit
+        """
+        return (self.match_turn - self.last_repair_turn) / GAME_CONSTANTS['PARAMETERS']['BREAKDOWN_TURNS'];
+    def move(self, dir):
+        return 'm {} {}'.format(self.id, dir)
+
+class Player:
+    energium: int
+    team: int
+    units: List[Unit]
+    bases: List[Base]
+    def __init__(self, team):
+        self.team = team
+        self.bases = []
+        self.units = []
+        self.energium = 0
---
+++ energium/position.py
@@ -0,0 +1,60 @@
+from .game_constants import DIRECTIONS, GAME_CONSTANTS
+import math
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def equals(self, opos):
+        return self.x == opos.x and self.y == opos.y
+
+    def is_adjacent(self, pos):
+        """
+        returns true if this position is adjacent to pos
+        """
+        dx = self.x - pos.x
+        dy = self.y - pos.y
+        if (abs(dx) + abs(dy) > 1):
+            return False
+        return True
+    def translate(self, direction, units):
+        """
+        translate a position in a direction by some units and returns a translated
+        Position
+        """
+        if direction == DIRECTIONS.NORTH:
+            return Position(self.x, self.y - units)
+        elif direction == DIRECTIONS.EAST:
+            return Position(self.x + units, self.y)
+        elif direction == DIRECTIONS.SOUTH:
+            return Position(self.x, self.y + units)
+        elif direction == DIRECTIONS.WEST:
+            return Position(self.x - units, self.y)
+
+    def distance_to(self, pos):
+        """
+        returns euclidean distance to the pos from this position
+        """
+        dx = pos.x - self.x
+        dy = pos.y - self.y
+        return math.sqrt(dx * dx + dy * dy)
+
+    def direction_to(self, targetPos):
+        """
+        gives direction that moves closest to targetPos from this position or None if staying put is closer
+        """
+        checkDirections = [
+            DIRECTIONS.NORTH,
+            DIRECTIONS.EAST,
+            DIRECTIONS.SOUTH,
+            DIRECTIONS.WEST,
+        ]
+        closestDirection = None
+        closestDist = self.distance_to(targetPos)
+        for dir in checkDirections:
+            newpos = self.translate(dir, 1)
+            dist = targetPos.distance_to(newpos)
+            if (dist < closestDist):
+                closestDist = dist
+                closestDirection = dir
+        return closestDirection
